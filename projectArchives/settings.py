2a895faf98ab3172ed1c16f8a374e2d744d284fb
raphael08 <rsiphael@gmail.com>
2023-03-14 22:57:26 +0300

    static

--- projectArchives/settings.py
+++ projectArchives/settings.py
@@ -139,7 +139,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
