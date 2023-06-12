2c686ffb03afb904ee122e78d4f223436419057e
raphael08 <rsiphael@gmail.com>
2023-06-12 10:33:57 +0300

    .

--- archives/admin.py
+++ archives/admin.py
@@ -5,3 +5,6 @@ from .models import *
 admin.site.register(Progress)
 admin.site.register(Staff)
 admin.site.register(Submission)
+admin.site.register(Document)
+admin.site.register(Student)
+admin.site.register(Project)
